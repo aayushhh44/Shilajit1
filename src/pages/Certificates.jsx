73c8f27fa67eabe089d07195e9b156f830938239
aayushhh44 <aayushpoudel59@gmail.com>
2024-05-10 13:56:54 +0545

    Add:jpg images

--- src/pages/Certificates.jsx
+++ src/pages/Certificates.jsx
@@ -19,13 +19,13 @@ const Certificates = () => {
     <div className='p-12'>
         <h1 className='text-2xl md:text-3xl font-poppins mb-4'>Our Certificates</h1>
       <div className='flex gap-4 w-full'>
-        <img onContextMenu="return false" className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/1.png" alt="Gmp Certificate" />
-        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/2.png" alt="haccp" />
+        <img onContextMenu="return false" className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/1.jpg" alt="Gmp Certificate" />
+        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/2.jpg" alt="haccp" />
       </div>
 
       <div className='flex gap-4 w-full mt-4'>
-      <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/3.png" alt="Kosher" />
-        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/4.png" alt="Halal" />
+      <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/3.jpg" alt="Kosher" />
+        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/4.jpg" alt="Halal" />
       </div>
     </div>
   )

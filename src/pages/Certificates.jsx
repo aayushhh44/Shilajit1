e102c6f2dd6f601ab197a30e806246014cfe5104
aayushhh44 <aayushpoudel59@gmail.com>
2024-05-10 13:53:42 +0545

    Fix:Copyrights and all

--- src/pages/Certificates.jsx
+++ src/pages/Certificates.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 
 const Certificates = () => {
+
+
+    useEffect(() => {
+        const handleContextmenu = e => {
+            e.preventDefault()
+        }
+        document.addEventListener('contextmenu', handleContextmenu)
+
+        return function cleanup() {
+            document.removeEventListener('contextmenu', handleContextmenu)
+        }
+}, [ ])
+
   return (
     <div className='p-12'>
         <h1 className='text-2xl md:text-3xl font-poppins mb-4'>Our Certificates</h1>
       <div className='flex gap-4 w-full'>
-        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/gmpcerti.png" alt="Gmp Certificate" />
-        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/haccp.jpg" alt="haccp" />
+        <img onContextMenu="return false" className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/1.png" alt="Gmp Certificate" />
+        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/2.png" alt="haccp" />
       </div>
 
       <div className='flex gap-4 w-full mt-4'>
-      <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/kosher.jpg" alt="Kosher" />
-        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/halal.jpg" alt="Halal" />
+      <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/3.png" alt="Kosher" />
+        <img className='w-1/2 border-4 rounded-md border-red-800' src="/assets/copyright/4.png" alt="Halal" />
       </div>
     </div>
   )
